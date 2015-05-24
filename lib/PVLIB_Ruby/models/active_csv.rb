70fa854819b2e00ef03e1b1001686cfc7a4829a4
Tadatoshi Takahashi <tadatoshi@Sansfil-Securise-Etudiants-Lassonde-244-132.polymtl.ca>
2015-05-24 18:17:40 -0400

    Cleaned up ActiveCsv code.

--- lib/PVLIB_Ruby/models/active_csv.rb
+++ lib/PVLIB_Ruby/models/active_csv.rb
@@ -14,37 +14,18 @@ class ActiveCsv
   end
 
   def valid?
-
-    binding.pry
-
-    record_attributes = load_data
-    # import_records = record_attributes.map {|attrs| MyModel.new(attrs)}
-    # import_records.map(&:valid?).all?
+    # TODO: implement this
   end
 
   def load_data
-    # csv = CSV.new(File.new(@csv_filepath), headers: true, return_headers: true, header_converters: :symbol)
     csv = CSV.new(File.new(@csv_filepath), headers: true, header_converters: :symbol)
 
     data = csv.shift
-    puts "data: #{data}"
-    puts "data.headers: #{data.headers}"
 
     data.headers.each do |header|
-      puts "data[header]: #{data[header]}"
-
       self.send("#{header}=".to_sym, data[header]) unless "#{header}=" == "="
     end
 
-    # CSV.foreach(@csv_filepath, headers: true, header_converters: :symbol) do |row|
-      
-    #   puts "row: #{row}"
-    #   puts "row.headers: #{row.headers}"
-
-    #   break;
-
-    # end
-    # return some useful data for making records
   end  
 
 end
