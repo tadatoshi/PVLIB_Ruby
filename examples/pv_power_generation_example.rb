38874a1f09e8395baf95d6c24e6b6c34bd20c63f
Tadatoshi Takahashi <tadatoshi@gmail.com>
2015-06-17 12:04:44 -0400

    Added Solar Ephemeris calculation to examples/pv_power_generation_example.rb.

--- examples/pv_power_generation_example.rb
+++ examples/pv_power_generation_example.rb
@@ -18,24 +18,15 @@ utc_offset = '-07:00'
 time = Time.new(2008, 10, 20, 11, 58, 12, utc_offset)
 albedo = BigDecimal('0.1500')
 array_tilt = BigDecimal('35')
-array_azimuth = BigDecimal('180')   
-
-sun_zenith = BigDecimal('45.7486')
-sun_azimuth = BigDecimal('182.5229')
-
-
-
+array_azimuth = BigDecimal('180')
 latitude = BigDecimal('35.0500')
 longitude = BigDecimal('-106.5400')
 altitude = BigDecimal('1660')
 location = Location.new(latitude, longitude, altitude)
-
 pressure = BigDecimal('62963')
 reference_solar_irradiance = BigDecimal('1000')
 wind_speed = BigDecimal('2.8786')
 air_temperature = BigDecimal('20.7700')
-
-
 
 solar_ephemeris = SolarEphemeris.new(time, location, pressure: pressure, temperature: air_temperature)
 
@@ -43,12 +34,12 @@ sun_azimuth = solar_ephemeris.sun_azimuth
 apparent_sun_elevation = solar_ephemeris.apparent_sun_elevation
 
 puts "------------- Solar Ephemeris -------------"
-puts "  Sun Azimuth [º]: #{sun_azimuth.round(4).to_s('F')}" # TODO: Doesn't Match to 182.5229 for 360th row in PVSC40Tutorial_Master
-puts "  Apparent Sun Elevation [º]: #{sun_zenith.round(4).to_s('F')}" # for 360th row in PVSC40Tutorial_Master
+puts "  Sun Azimuth [º]: #{sun_azimuth.round(4).to_s('F')}" # Slight difference from 182.5229 for 360th row in PVSC40Tutorial_Master
+puts "  Apparent Sun Elevation [º]: #{apparent_sun_elevation.round(4).to_s('F')}" # Slight difference from 44.2308 for 360th row in PVSC40Tutorial_Master
 puts "-------------------------------------------"
 puts ""
 
-sun_azimuth = BigDecimal('90') - apparent_sun_elevation
+sun_zenith = BigDecimal('90') - apparent_sun_elevation
 
 plain_of_array_irradiance = PlainOfArrayIrradiance.new(direct_normal_irradiance, global_horizontal_irradiance, diffuse_horizontal_irradiance, day_of_year, albedo, array_tilt, array_azimuth, sun_zenith, sun_azimuth)    
 
@@ -59,11 +50,11 @@ ground_diffuse_irradiance = plain_of_array_irradiance.ground_diffuse_irradiance
 sky_diffuse_irradiance = plain_of_array_irradiance.sky_diffuse_irradiance
 
 puts "----- Plain Of Array (POA) Irradiance -----"
-puts "  Angle of Incidence [º]: #{angle_of_incidence.round(4).to_s('F')}" # Matches to 10.8703 for 360th row in PVSC40Tutorial_Master
+puts "  Angle of Incidence [º]: #{angle_of_incidence.round(4).to_s('F')}" # Slight difference from 10.8703 for 360th row in PVSC40Tutorial_Master
 puts ""
-puts "  Beam Irradiance [W/m^2]: #{beam_irradiance.round(4).to_s('F')}" # Matches to 619.9822 for 360th row in PVSC40Tutorial_Master
+puts "  Beam Irradiance [W/m^2]: #{beam_irradiance.round(4).to_s('F')}" # Slight difference from 619.9822 for 360th row in PVSC40Tutorial_Master
 puts "  Ground Diffuse Irradiance [W/m^2]: #{ground_diffuse_irradiance.round(4).to_s('F')}" # Matches to 11.6927 for 360th row in PVSC40Tutorial_Master
-puts "  Sky Diffuse Irradiance [W/m^2]: #{sky_diffuse_irradiance.round(4).to_s('F')}" # Matches to 464.8004 for 360th row in PVSC40Tutorial_Master
+puts "  Sky Diffuse Irradiance [W/m^2]: #{sky_diffuse_irradiance.round(4).to_s('F')}" # Slight difference from 464.8004 for 360th row in PVSC40Tutorial_Master
 puts "-------------------------------------------"
 puts ""
 
@@ -76,7 +67,7 @@ pv_temperature = PvTemperature.new(sandia_pv_module, solar_irradiance_incident_o
 
 puts "------------- PV temperature --------------"
 puts "  Estimated Cell Temperature [ºC]: #{pv_temperature.cell_temperature.round(4).to_s('F')}" # Slight difference from 47.7235 for 360th row in PVSC40Tutorial_Master
-puts "  Estimated Module Temperature [ºC]: #{pv_temperature.module_temperature.round(4).to_s('F')}" # Matches to 44.4341 for 360th row in PVSC40Tutorial_Master
+puts "  Estimated Module Temperature [ºC]: #{pv_temperature.module_temperature.round(4).to_s('F')}" # Slight difference from 44.4341 for 360th row in PVSC40Tutorial_Master
 puts "-------------------------------------------"
 puts ""
 
@@ -85,7 +76,7 @@ estimated_cell_temperature = pv_temperature.cell_temperature
 air_mass = AirMass.new(sun_zenith, pressure)
 
 puts "---------------- Air Mass -----------------"
-puts "  Absolute Air Mass: #{air_mass.absolute_air_mass.round(4).to_s('F')}"
+puts "  Absolute Air Mass: #{air_mass.absolute_air_mass.round(4).to_s('F')}" # Slight difference from 0.8894 for 360th row in PVSC40Tutorial_Master
 puts "-------------------------------------------"
 puts ""
 
@@ -96,12 +87,12 @@ pv_performance_characterization = PvPerformanceCharacterization.new(sandia_pv_mo
                                                                     estimated_cell_temperature)
 
 puts "--- PV module IV curve characterization ---"
-puts "  Short Circuit Current [A]: #{pv_performance_characterization.short_circuit_current.round(4).to_s('F')}"
-puts "  Maximum Power Point Current [A]: #{pv_performance_characterization.maximum_power_point_current.round(4).to_s('F')}"
-puts "  Open Circuit Voltage [V]: #{pv_performance_characterization.open_circuit_voltage.round(4).to_s('F')}"
-puts "  Maximum Power Point Voltage [V]: #{pv_performance_characterization.maximum_power_point_voltage.round(4).to_s('F')}"
-puts "  Fourth Point Current [A]: #{pv_performance_characterization.fourth_point_current.round(4).to_s('F')}"
-puts "  Fifth Point Current [A]: #{pv_performance_characterization.fifth_point_current.round(4).to_s('F')}"
+puts "  Short Circuit Current [A]: #{pv_performance_characterization.short_circuit_current.round(4).to_s('F')}" # Slight difference from 6.4151 for 360th row in PVSC40Tutorial_Master
+puts "  Maximum Power Point Current [A]: #{pv_performance_characterization.maximum_power_point_current.round(4).to_s('F')}" # Matches to 5.8741 for 360th row in PVSC40Tutorial_Master
+puts "  Open Circuit Voltage [V]: #{pv_performance_characterization.open_circuit_voltage.round(4).to_s('F')}" # Matches to 45.2333 for 360th row in PVSC40Tutorial_Master
+puts "  Maximum Power Point Voltage [V]: #{pv_performance_characterization.maximum_power_point_voltage.round(4).to_s('F')}" # Matches to 36.3984 for 360th row in PVSC40Tutorial_Master
+puts "  Fourth Point Current [A]: #{pv_performance_characterization.fourth_point_current.round(4).to_s('F')}" # Matches to 6.3544 for 360th row in PVSC40Tutorial_Master
+puts "  Fifth Point Current [A]: #{pv_performance_characterization.fifth_point_current.round(4).to_s('F')}" # Slight difference from 4.3899 for 360th row in PVSC40Tutorial_Master
 puts "-------------------------------------------"
 puts ""
 
@@ -113,7 +104,7 @@ array_power = array_current * array_voltage
 
 puts "------------- PV array output -------------"
 puts "  Array current [A]: #{array_current.round(4).to_s('F')}" # Matches to 5.8741 for 360th row in PVSC40Tutorial_Master
-puts "  Array voltage [V]: #{array_voltage.round(4).to_s('F')}" # Matches to 181.9919 for 360th row in PVSC40Tutorial_Master
+puts "  Array voltage [V]: #{array_voltage.round(4).to_s('F')}" # Slight difference from 181.9919 for 360th row in PVSC40Tutorial_Master
 puts "  Array power [W]: #{array_power.round(4).to_s('F')}" # Matches to 1069.0 (with rounding) for 360th row in PVSC40Tutorial_Master
 puts "-------------------------------------------"
 puts ""
